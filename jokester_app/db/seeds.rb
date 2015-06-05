7ac2b6861be313c3a247e491185f49433e5a0d48
Walker Flieller <wflieller@gmail.com>
2015-06-05 14:31:43 -0500

    Changes to users

--- jokester_app/db/seeds.rb
+++ jokester_app/db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+# => 
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create username: 'walker', email:  'walker', first_name: 'walker', last_name: 'walker', password: 'walker'
