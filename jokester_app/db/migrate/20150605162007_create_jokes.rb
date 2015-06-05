3d30a14782ff7f2672c3ac0aa7bacdbdcc0ea99f
Max DePriest <maxd924@gmail.com>
2015-06-05 11:36:44 -0500

    Adding to migrations

--- jokester_app/db/migrate/20150605162007_create_jokes.rb
+++ jokester_app/db/migrate/20150605162007_create_jokes.rb
@@ -1,8 +1,8 @@
 class CreateJokes < ActiveRecord::Migration
   def change
     create_table :jokes do |t|
-      t.text :question
-      t.text :answer
+      t.text :question, :null => false
+      t.text :answer, :null => false
     end
   end
 end
--- jokester_app/db/migrate/20150605162349_create_users.rb
+++ jokester_app/db/migrate/20150605162349_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password
-      t.string :first_name
-      t.string :last_name
+      t.string :username, :null => false, :unique => true, :index => true
+      t.string :email, :null => false, :unique => true
+      t.string :password, :null => false
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
     end
   end
 end
